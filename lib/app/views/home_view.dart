8b8a5d866e7b974adb6920ea552c45af8ba39c11
EnzoCabrera <enzo2005cabrera@gmail.com>
2024-04-26 17:44:14 -0300

    HomePage

---
+++ lib/app/views/home_view.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Padding(
+          padding:
+              const EdgeInsets.only(left: 20, right: 20, top: 100, bottom: 20),
+          child: Column(
+            children: [
+              Image.asset(
+                'assets/logo.png',
+                width: 150,
+                height: 150,
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                        flex: 1,
+                        child: SizedBox(
+                          height: 56,
+                          child: DropdownButton(
+                              isExpanded: true,
+                              underline: Container(
+                                height: 1,
+                                color: Colors.amber,
+                              ),
+                              items: [
+                                DropdownMenuItem(
+                                    value: 1,
+                                    child: Text(
+                                      'Real',
+                                      overflow: TextOverflow.ellipsis,
+                                    )),
+                                DropdownMenuItem(
+                                    value: 1,
+                                    child: Text(
+                                      'Dolar',
+                                      overflow: TextOverflow.ellipsis,
+                                    )),
+                              ],
+                              onChanged: (value) {}),
+                        )),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Expanded(
+                      flex: 2,
+                      child: TextField(
+                        decoration: InputDecoration(
+                            enabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.amber),
+                            ),
+                            focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.amber))),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 50),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('CONVERTER'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
